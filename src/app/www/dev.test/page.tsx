af11a0fcd6892373c2ea89bbabd4942883bf5f6e
Khalid Kaiser <romman.kaiser@gmail.com>
2024-11-10 22:42:56 +0600

    ui-header

--- src/app/www/dev.test/page.tsx
+++ src/app/www/dev.test/page.tsx
@@ -1,10 +1,25 @@
 import Sticky from '@/app/_ui/footers/Sticky'
+import Default from '@/app/_ui/headers/Default'
 import Link from 'next/link'
 import React from 'react'
 
 function Home() {
   return (
     <div>
+        <Default companyName='Dreamer'>
+        <li>
+                        <Link href="#" className="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white" aria-current="page">Home</Link>
+                    </li>
+                    <li>
+                        <Link href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Marketplace</Link>
+                    </li>
+                    <li>
+                        <Link href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Team</Link>
+                    </li>
+                    <li>
+                        <Link href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">About Us</Link>
+                    </li>
+        </Default>
       <h1>This is dev test</h1>
       <Sticky companyName='Dreamer'>
         <li>
